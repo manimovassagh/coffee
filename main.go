a3630c52c36b8776f9290047eee05be858dc5f90
Mani Movassagh <mani.mgh@gmail.com>
2024-07-08 21:31:50 +0200

    working

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/manimovassagh/coffee/types"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("This is working!!")
+	var db *gorm.DB
+	var err error
+
+	// Retry connecting to the database
+	for i := 0; i < 10; i++ {
+		dsn := "user:password@tcp(localhost:3306)/coffee_app?charset=utf8mb4&parseTime=True&loc=Local"
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("Failed to connect to database. Retrying... (%d/10)", i+1)
+		time.Sleep(5 * time.Second)
+	}
+	if err != nil {
+		log.Fatal("Could not connect to the database. Exiting...")
+	}
+
+	// Auto migrate the schema
+	err = db.AutoMigrate(&types.Role{}, &types.User{}, &types.Category{}, &types.Product{}, &types.Order{}, &types.OrderItem{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Database migration completed successfully!")
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to the Coffee App!")
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
 }
---
+++ types/types.go
@@ -0,0 +1,50 @@
+package types
+
+type Role struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"type:enum('buyer', 'seller');not null"`
+}
+
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Username string `gorm:"unique;not null"`
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	RoleID   uint
+	Role     Role `gorm:"foreignKey:RoleID"`
+}
+
+type Category struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"not null"`
+}
+
+type Product struct {
+	ID          uint   `gorm:"primaryKey"`
+	Name        string `gorm:"not null"`
+	Description string
+	Price       float64 `gorm:"not null"`
+	CategoryID  uint
+	Category    Category `gorm:"foreignKey:CategoryID"`
+	UserID      uint
+	User        User `gorm:"foreignKey:UserID"`
+}
+
+type Order struct {
+	ID        uint `gorm:"primaryKey"`
+	UserID    uint
+	User      User    `gorm:"foreignKey:UserID"`
+	Total     float64 `gorm:"not null"`
+	Status    string  `gorm:"not null"`
+	CreatedAt uint64  `gorm:"autoCreateTime"`
+}
+
+type OrderItem struct {
+	ID        uint `gorm:"primaryKey"`
+	OrderID   uint
+	Order     Order `gorm:"foreignKey:OrderID"`
+	ProductID uint
+	Product   Product `gorm:"foreignKey:ProductID"`
+	Quantity  int     `gorm:"not null"`
+	Price     float64 `gorm:"not null"`
+}
